2e4aa4df88ed374805831da4823869dc9055e318
Sebastian Hönel <sebastian.honel@softwerk.se>
2016-01-02 19:17:36 +0100

    Update index.ts to use state- and authorization-routes

---
+++ index.ts
@@ -0,0 +1,21 @@
+/// <reference path="./typings/express/express.d.ts" />
+import Express = require('express');
+
+// We load the OAuth-client-secret from the environment and immediately
+// end the application if it doesn't exist.
+const OAUTH_CLIENT_ID = process.env.OAUTH_CLIENT_ID || null;
+const OAUTH_CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET || null;
+if (OAUTH_CLIENT_ID === null || OAUTH_CLIENT_SECRET === null) {
+	process.exit(1);
+}
+
+
+var app: Express.Express = Express();
+
+import route_auth = require('./routes/authorize');
+import route_states = require('./routes/states');
+
+route_auth.configure(app, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET);
+route_states.configure(app);
+
+app.listen(process.env.PORT || 80);
