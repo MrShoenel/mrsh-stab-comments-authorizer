4bce8975bfb6f5cdc32ad55054c045cb8f9e2da7
MrShoenel <development@hoenel.net>
2016-01-06 04:23:26 +0100

    Add the conditional parameter 'authorize_at' to the states route and increment version to v1.4.3

--- routes/states.ts
+++ routes/states.ts
@@ -7,9 +7,21 @@ export function configure(expressApp: Express.Express) {
 	/**
 	 * This route should be used to retrieve an unguessable state which
 	 * will be valid for ten minutes.
+	 * If the request contains the parameter 'authorize_at' then we will
+	 * redirect to that URL and append the state as parameter.
 	 */
 	expressApp.get('/state', (req: Express.Request, res: Express.Response) => {
-		res.type('text/plain');
-		res.end(StateResource.getState());
+		// If the following is set, we append the state as paramete and redirect.
+		const authorizeAtUrl = req.param('authorize_at', null);
+
+		if (authorizeAtUrl === null) {
+			res.type('text/plain');
+			res.end(StateResource.getState());
+		} else {
+			const decoded = decodeURIComponent(authorizeAtUrl);
+			const queryOp = decoded.indexOf('?') >= 0 ? '&' : '?';
+			res.redirect(decoded + queryOp + 'state=' + StateResource.getState());
+			res.end();
+		}
 	});
 };
